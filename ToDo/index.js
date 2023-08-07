45c88f6789f6404dd6d2c9aae08dc811ada738ab
Muhammad Habib Khan <muhammad2002khan@gmail.com>
2023-08-07 18:32:21 +0500

    ToDo List CRUD Complete. Verification and Validation for each user updating their own data needs work. Final Assignment of Mern Stack Training.

---
+++ ToDo/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./Router/toDo');
+const app = express();
+const port = 3000;
+
+//Check git
+
+
+// Middleware
+app.use(express.json());
+// MongoDB connection URL
+const url = 'mongodb://localhost:27017/ToDo';
+// Connect to MongoDB
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+// Routes
+app.use('/', router);
